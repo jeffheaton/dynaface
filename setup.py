1a0cd0a7f8c8bbacb9218c19e415ab8d6b3b80f1
Jeff Heaton <jeff@jeffheaton.com>
2023-06-19 05:54:47 -0500

    Update requirements

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(
     author_email='author@gmail.com',
     description='Description of my package',
     packages=find_packages(),    
-    install_requires=["dlib>=19.24.1","opencv-python>=4.7.0","Pillow>=8.4.0","matplotlib>=3.7.1","scikit-learn>=1.2.2"], 
+    install_requires=["dlib>=19.24.1","opencv-python>=4.7.0","Pillow>=8.4.0","matplotlib>=3.7.1","scikit-learn>=1.2.2","plotly>=5.9.0","kaleido>=0.2.1"], 
 )
