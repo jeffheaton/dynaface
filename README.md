0f76e2ace63d4efbaedc50e2b97b4f094c460f4c
Jeff Heaton <jeff@jeffheaton.com>
2023-06-19 09:03:32 -0500

    Added updates to build whl file.

--- README.md
+++ README.md
@@ -18,4 +18,6 @@ https://www.geeksforgeeks.org/using-jupyter-notebook-in-virtual-environment/
 
 ipython kernel install --user --name=venv
 
+python setup.py sdist bdist_wheel
+
 cp -R /content/drive/MyDrive/facial_analysis/ /content
--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='facial_analysis',
     version='0.1.0',
-    url='your-repo-url-if-any',
-    author='Author Name',
-    author_email='author@gmail.com',
+    url='https://github.com/jeffheaton/facial-analysis',
+    author='Jeff Heaton',
+    author_email='jeff@jeffheaton.com',
     description='Description of my package',
     packages=find_packages(),    
     install_requires=["dlib>=19.24.1","opencv-python>=4.7.0","Pillow>=8.4.0","matplotlib>=3.7.1","scikit-learn>=1.2.2","plotly>=5.9.0","kaleido>=0.2.1"], 
