70d0a1440866d729497e03e7e712c16d2e31716b
Jeff Heaton <jeff@jeffheaton.com>
2023-11-25 20:25:34 -0600

    fixed issue with some sized images

--- dynaface-app/dynaface_app.py
+++ dynaface-app/dynaface_app.py
@@ -5,21 +5,10 @@ import logging.handlers
 import os
 import plistlib
 import sys
-import webbrowser
 
 import torch
 from facial_analysis.find_face import FindFace
-from PyQt6.QtCore import QTimer
-from PyQt6.QtGui import QAction, QKeySequence
-from PyQt6.QtWidgets import QFileDialog, QMenu, QMenuBar, QTabWidget
-
-import const_values
-import tab_settings
-import tab_splash
 from jth_ui.app_jth import AppJTH
-from jth_ui.window_jth import MainWindowJTH
-from tab_about import AboutTab
-from tab_analyze_image import AnalyzeImageTab
 from dynaface_window import DynafaceWindow
 
 logger = logging.getLogger(__name__)
