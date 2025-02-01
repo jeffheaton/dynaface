89ef253fc3f5705c0eb0f8e40448a0729524170b
Jeff Heaton <jeff@jeffheaton.com>
2025-02-01 16:20:43 -0600

    Update build to lock on Python 3.12, also update requirements to latest versions of stuff

--- dynaface-app/deploy/macos/build.sh
+++ dynaface-app/deploy/macos/build.sh
@@ -18,7 +18,7 @@ fi
 # Environment
 cd ../..
 rm -rf ./venv || true
-python3 -m venv venv
+python3.12 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 pip install facial_analysis -f ./wheels
