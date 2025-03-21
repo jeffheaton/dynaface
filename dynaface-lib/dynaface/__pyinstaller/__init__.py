b4691aaced52ba5dec24e246e0b1760a18879762
Jeff Heaton <jeff@jeffheaton.com>
2025-03-21 17:26:35 -0500

    Try to autoset __version__

--- dynaface-lib/dynaface/__pyinstaller/__init__.py
+++ dynaface-lib/dynaface/__pyinstaller/__init__.py
@@ -1,5 +1,12 @@
 import os
 
+try:
+    from importlib.metadata import version
+
+    __version__ = version(__name__)
+except:
+    __version__ = "unknown"
+
 
 def get_hook_dirs():
     return [os.path.dirname(__file__)]
