d7a3cd54aa1194d26dceb3a7a29436b07fa2b672
Jeff Heaton <jeff@jeffheaton.com>
2025-03-20 21:54:01 -0400

    Update dynaface lib hooks

--- dynaface-app/deploy/macos/build.sh
+++ dynaface-app/deploy/macos/build.sh
@@ -20,7 +20,7 @@ rm -rf ./venv || true
 python3.11 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
-pip install --upgrade https://data.heatonresearch.com/library/dynaface-0.1.5-py3-none-any.whl
+pip install --upgrade https://data.heatonresearch.com/library/dynaface-0.1.6-py3-none-any.whl
 cd deploy/macos
 
 # Build it
--- dynaface-lib/dynaface/__init__.py
+++ dynaface-lib/dynaface/__init__.py
@@ -1,3 +1,3 @@
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 
 from .models import init_models, are_models_init, detect_device
