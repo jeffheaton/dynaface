7d53851c157c1b64fbefe9d3063ecb8fc42caa8b
Jeff Heaton <jeff@jeffheaton.com>
2025-04-22 09:11:40 -0500

    Update build with constraints

--- dynaface-app/deploy/windows/build.ps1
+++ dynaface-app/deploy/windows/build.ps1
@@ -20,7 +20,7 @@ try {
     & "./venv/Scripts/Activate.ps1"
 
     # Install dependencies
-    pip install -r requirements.txt
+    pip install -r requirements.txt --constraint constraints.txt
     pip install --upgrade dynaface
     # pip install --force-reinstall --upgrade onnxruntime==1.20.1
     pip install --upgrade https://data.heatonresearch.com/library/dynaface-0.2.2-py3-none-any.whl
