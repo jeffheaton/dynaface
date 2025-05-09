7a2001dbc9c3f519dda28ca3ff162ca23c80923d
Jeff Heaton <jeff@jeffheaton.com>
2025-05-09 11:18:44 -0500

    Add constraints for pyqt6, latest version causes QFileDialog issues

--- build_debug.sh
+++ build_debug.sh
@@ -3,6 +3,7 @@ rm -rf ./venv || true
 python3.11 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
+pip install -r requirements.txt --constraint ../constraints.txt
 
 # Download and unzip dynaface_models.zip
 mkdir -p ./data
